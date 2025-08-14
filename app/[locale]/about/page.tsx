e0136483b11bbaa821f5371cd3e716ee3f0d2841
Yayo <andreshernandez@Yayos-MacBook-Pro.local>
2025-08-14 11:00:36 -0400

    Fixing about page params

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,20 +1,6 @@
-import { ReactNode } from 'react';
-import { notFound } from 'next/navigation';
-import esMessages from '@/locales/es/common.json';
-import enMessages from '@/locales/en/common.json';
+'use client';
+import { t } from '@/components/LocaleProvider';
 
-export const generateStaticParams = () => [{ locale: 'es' }, { locale: 'en' }];
-
-export default function LocaleLayout({
-  params,
-}: {
-  params: { locale: string };
-  children: ReactNode;
-}) {
-  const { locale } = params;
-  const messages = locale === 'es' ? esMessages : locale === 'en' ? enMessages : null;
-
-  if (!messages) return notFound();
-
-  return <h1>About me</h1>;
+export default function LocaleLayout() {
+  return <h1>{t('navbar_about')}</h1>;
 }
